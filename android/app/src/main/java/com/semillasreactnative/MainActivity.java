5909cb56f50ed3cbfe2d85994e964b38ecf372e7
Ismael de Esteban <ismael@feverup.com>
2017-03-24 09:43:56 +0100

    Implemented fixes sugested by @brascene

--- android/app/src/main/java/com/semillasreactnative/MainActivity.java
+++ android/app/src/main/java/com/semillasreactnative/MainActivity.java
@@ -1,15 +1,35 @@
 package com.semillasreactnative;
 
 import com.facebook.react.ReactActivity;
+import com.proxima.RCTDigits.DigitsPackage;
+import com.digits.sdk.android.Digits;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+
+import com.twitter.sdk.android.core.TwitterCore;
+import com.digits.sdk.android.*;
 
 public class MainActivity extends ReactActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
+    private static final String TWITTER_KEY = "placeholder";
+    private static final String TWITTER_SECRET = "placeholder";
+
     @Override
     protected String getMainComponentName() {
         return "SemillasReactNative";
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+          Fabric.with(this, new TwitterCore(authConfig), new Digits.Builder().build());
+        }
 }
+
