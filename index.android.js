0c2e77152e5e6de73bc55b46bc36244af60cad5c
Ismael de Esteban <ismael.deesteban@gmail.com>
2017-09-03 08:20:14 +0200

    proper sentry configuration

--- index.android.js
+++ index.android.js
@@ -5,6 +5,6 @@ import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
 import { Sentry } from 'react-native-sentry';
 
-Sentry.config('https://2d3dcdcb83e04977ba15535574bb87d5:903189fa421f415f8b7f5f3ee0780473@sentry.io/197460').install();
+Sentry.config('https://70a43f2d956b42db966bc72a7849db7a:ba4a4754671e4d3cb28bc4e02b6560de@sentry.io/210067').install();
 
 AppRegistry.registerComponent('FaircoinDiscovery', () => App)
